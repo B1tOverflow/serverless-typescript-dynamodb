fece58f397ae67ba8cd9dd4b6fda01d0e5bf3a8c
Sachin Thakur <thakursachin467@gmail.com>
2020-09-06 20:13:40 +0530

    services and controller with dynamodb added

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -1,21 +1,90 @@
+import awsSdk from "aws-sdk";
+const dynamoDb = new awsSdk.DynamoDB.DocumentClient();
+
 export class ToDoService {
-  protected async createTodo(params): Promise<any> {
-    return {};
+  protected async create(params): Promise<any> {
+    try {
+      const result = await dynamoDb
+        .put({
+          TableName: process.env.TODO_TABLE_NAME,
+          Item: params,
+        })
+        .promise();
+      const data = result.$response.data;
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
-  protected async findTodo(params): Promise<any> {
-    return {};
+  protected async find(id): Promise<any> {
+    try {
+      const result = await dynamoDb
+        .get({
+          TableName: process.env.TODO_TABLE_NAME,
+          Key: {
+            id,
+          },
+        })
+        .promise();
+      const data = result.Item;
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
-  protected async findTodos(params): Promise<any[]> {
-    return [{}];
+  protected async findAll(): Promise<any[]> {
+    try {
+      const result = await dynamoDb
+        .scan({
+          TableName: process.env.TODO_TABLE_NAME,
+        })
+        .promise();
+      const data = result.Items;
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
-  protected async updateTodo(params): Promise<any> {
-    return {};
+  protected async update(params): Promise<any> {
+    const { key, UpdateExpression, ExpressionAttributeValues } = params;
+    try {
+      const result = await dynamoDb
+        .update({
+          TableName: process.env.TODO_TABLE_NAME,
+          Key: {
+            ...key,
+          },
+          UpdateExpression: UpdateExpression,
+          ExpressionAttributeValues: {
+            ...ExpressionAttributeValues,
+          },
+          ReturnValues: "ALL_NEW", // fetch all new and updated values
+        })
+        .promise();
+      const data = result.Attributes;
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 
-  protected async deleteTodo(params): Promise<any> {
-    return {};
+  protected async delete(id): Promise<any> {
+    try {
+      const result = await dynamoDb
+        .delete({
+          TableName: process.env.TODO_TABLE_NAME,
+          Key: {
+            id,
+          },
+        })
+        .promise();
+      const data = result.Attributes;
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 }
